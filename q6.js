4594b34c0505018df34347da5f271d0c4a62ab34
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:17:58 +0500

    Add files via upload

---
+++ q6.js
@@ -0,0 +1,7 @@
+/* Ques #  06:
+Stripping name: store a person name, and including some white spaces characters  at the beginning and end of the name. Make sure you use each.
+Character combination,"\t" and "\n" at least once. Print the name once, so the whitespaces around the name is displayed. Then print the name after the striping the white spaces*/
+var person_Name = "Talha Haroon";
+console.log("  Talha Haroon ");
+console.log("Talha\tHaroon");
+console.log("Talha\n Haroon");
---
+++ q6.ts
@@ -0,0 +1,8 @@
+/* Ques #  06:
+Stripping name: store a person name, and including some white spaces characters  at the beginning and end of the name. Make sure you use each.
+Character combination,"\t" and "\n" at least once. Print the name once, so the whitespaces around the name is displayed. Then print the name after the striping the white spaces*/
+
+let person_Name:string = "Talha Haroon"
+console.log("  Talha Haroon ")
+console.log("Talha\tHaroon")
+console.log("Talha\n Haroon")
